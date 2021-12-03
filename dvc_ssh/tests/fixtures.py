4364562612b74a66cb6426027503e2642b87d19e
Ruslan Kuprieiev <kupruser@gmail.com>
2021-12-03 09:30:00 +0200

    cruft: update template

--- dvc_ssh/tests/fixtures.py
+++ dvc_ssh/tests/fixtures.py
@@ -7,9 +7,7 @@ from .cloud import SSH, TEST_SSH_KEY_PATH, TEST_SSH_USER
 
 @pytest.fixture(scope="session")
 def docker_compose_file(pytestconfig):
-    return os.path.join(
-        str(pytestconfig.rootdir), "dvc_ssh", "tests", "docker-compose.yml"
-    )
+    return os.path.join(os.path.dirname(__file__), "docker-compose.yml")
 
 
 @pytest.fixture(scope="session")
