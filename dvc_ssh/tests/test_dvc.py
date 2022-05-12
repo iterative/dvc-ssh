c7b0b8c54430fbf8955b54b9658e912d8c9d2572
Ruslan Kuprieiev <kupruser@gmail.com>
2022-05-12 20:30:37 +0300

    tests: don't skip import/add tests

--- dvc_ssh/tests/test_dvc.py
+++ dvc_ssh/tests/test_dvc.py
@@ -3,10 +3,8 @@ from dvc.testing.test_api import TestAPI  # noqa, pylint: disable=unused-import
 from dvc.testing.test_remote import (  # noqa, pylint: disable=unused-import
     TestRemote,
 )
-from dvc.testing.test_workspace import (  # noqa, pylint: disable=unused-import
-    TestAdd,
-    TestImport,
-)
+from dvc.testing.test_workspace import TestAdd as _TestAdd
+from dvc.testing.test_workspace import TestImport as _TestImport
 
 
 @pytest.fixture
@@ -24,31 +22,29 @@ def workspace(make_workspace, cloud_name):
     yield make_workspace(name="workspace", typ=cloud_name)
 
 
-@pytest.fixture
-def stage_md5():
-    return "dc24e1271084ee317ac3c2656fb8812b"
+class TestImport(_TestImport):
+    @pytest.fixture
+    def stage_md5(self):
+        return "dc24e1271084ee317ac3c2656fb8812b"
+
+    @pytest.fixture
+    def is_object_storage(self):
+        return False
+
+    @pytest.fixture
+    def dir_md5(self):
+        return "b6dcab6ccd17ca0a8bf4a215a37d14cc.dir"
 
 
-@pytest.fixture
-def is_object_storage():
-    return False
+class TestAdd(_TestAdd):
+    @pytest.fixture
+    def hash_name(self):
+        return "md5"
 
+    @pytest.fixture
+    def hash_value(self):
+        return "8c7dd922ad47494fc02c388e12c00eac"
 
-@pytest.fixture
-def dir_md5():
-    return "b6dcab6ccd17ca0a8bf4a215a37d14cc.dir"
-
-
-@pytest.fixture
-def hash_name():
-    return "md5"
-
-
-@pytest.fixture
-def hash_value():
-    return "8c7dd922ad47494fc02c388e12c00eac"
-
-
-@pytest.fixture
-def dir_hash_value(dir_md5):
-    return "b6dcab6ccd17ca0a8bf4a215a37d14cc.dir"
+    @pytest.fixture
+    def dir_hash_value(self):
+        return "b6dcab6ccd17ca0a8bf4a215a37d14cc.dir"
