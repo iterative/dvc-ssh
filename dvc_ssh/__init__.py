740cfaa0ca1725f6896cede5b45a3e99ca18424f
Sailesh <58628378+99-NinetyNine@users.noreply.github.com>
2025-01-07 07:56:18 +0545

    the setup works in prod; test cases might have to be changed

--- dvc_ssh/__init__.py
+++ dvc_ssh/__init__.py
@@ -52,11 +52,15 @@ class SSHFileSystem(FileSystem):
             "client_factory", InteractiveSSHClient
         )
         try:
+            assert config.get("host) is not None
             user_ssh_config = parse_config(
                 host=config["host"], port=config.get("port", DEFAULT_PORT)
             )
 
         except FileNotFoundError:
+            user_ssh_config = {}
+        except Exception:
+            # host could have been None
             user_ssh_config = {}
 
         login_info["host"] = user_ssh_config.get("Hostname", config["host"])
