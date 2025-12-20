7296ea1fb68f512e875c18847826410be9239f3e
Martin Wurzer <martin.wurzer@irubis.com>
2025-12-20 15:34:21 +0100

    clean tests;

--- dvc_ssh/tests/test_conn.py
+++ dvc_ssh/tests/test_conn.py
@@ -2,27 +2,8 @@ from dvc_ssh import SSHFileSystem
 
 from .cloud import TEST_SSH_KEY_PATH
 
-# TODO: InteractiveSSHClient.public_key_auth_requested
 
-# "ssh": {
-#     "type": supported_cache_type,
-#     "port": Coerce(int),
-#     "user": str,
-#     "password": str,
-#     "ask_password": Bool,
-#     "passphrase": str,
-#     "ask_passphrase": Bool,
-#     "keyfile": str,
-#     "timeout": Coerce(int),
-#     "gss_auth": Bool,
-#     "allow_agent": Bool,
-#     "max_sessions": Coerce(int),
-#     Optional("verify", default=False): Bool,
-#     **REMOTE_COMMON,
-# },
-
-
-def test_denied_user(ssh_server, mocker):
+def test_keyfile_set(ssh_server, mocker):
     f = SSHFileSystem(
         host=ssh_server["host"],
         port=ssh_server["port"],
